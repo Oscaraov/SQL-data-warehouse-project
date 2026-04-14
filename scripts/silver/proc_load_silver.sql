d95fa6d2dec7406ac4784c02b9d0bebc0767acb2
Oscaraov <oscar.lonnerheden@vaxjo.se>
2026-04-14 10:52:49 +0200

    Update proc_load_silver.sql
    Added silver.crm_prd_info

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -37,4 +37,36 @@ FROM
 	WHERE cst_id IS NOT NULL
 )t WHERE flag_last = 1 -- Select te most recent record for every customer
 
--- Continue with bronze.crm_prd_info
+-------------------------------------------------------------------
+	
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING (prd_key, 1, 5), '-','_') AS cat_id, -- Ersätter - med _ och transformerar för att kunna joina tabeller
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Behåller allt efter kategori ID för att kunna joina tabeller
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost, -- NULL ersätts med 0
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+		END AS prd_line, -- Ersätter kortnamn med läsbara namn och NULL med n/a
+	CAST(prd_start_dt AS DATE) AS prd_start_dt, -- Transformerar till DATE för att fåp bort onödig timestamp
+	CAST(
+		LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 
+		AS DATE
+	) AS prd_end_dt -- Ersätter slutdatum med dagen före nästa startdatum och transformerar till DATE för att fåp bort onödig timestamp
+FROM bronze.crm_prd_info
+
+-- Continue with bronze.crm_sales_details

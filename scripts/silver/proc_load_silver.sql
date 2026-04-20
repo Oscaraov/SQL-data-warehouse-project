9bf7e1c25f10e675f64a05078cb5e93936ceaeba
Oscaraov <oscar.lonnerheden@vaxjo.se>
2026-04-20 14:01:22 +0200

    Update proc_load_silver.sql
    updated silver.erp_px_cat_g1v2

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -145,5 +145,15 @@ FROM bronze.erp_loc_a101
 
 --------------------------------------------------------
 
-
--- Continue with bronze.erp_px_cat_g1v2
+INSERT INTO silver.erp_px_cat_g1v2(
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2

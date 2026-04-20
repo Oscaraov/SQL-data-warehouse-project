53d2f0886099273b972130203cb88ada0491f31b
Oscaraov <oscar.lonnerheden@vaxjo.se>
2026-04-20 14:46:12 +0200

    Update proc_load_silver.sql
    Updated entire stored procedure with timestamps and error catching

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,171 +1,241 @@
 /* 
 --------------------------
-Script for loading data from 'bronze' layer to 'silver' layer
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
 --------------------------
+Script purpose:
+	This stored procedure loads data into the 'silver' layer schema from the 'bronze' layer.
+	It performs the following actions:
+	- Truncates the silver tables before loading data.
+	- Uses the 'BULK INSERT' command to load data from the bronze tables to the silver tables.
+	
+	Parameters:
+		None.
+	This stored procedure does not accept any parameters or return any values.
 
+	Usage example:
+		EXEC silver.load_silver;
+
+	--------------------------
 */
-PRINT '>> Truncating Table: silver.crm_cust_info';
-TRUNCATE TABLE silver.crm_cust_info;
-PRINT '>> Inseting Data Into: silver.crm_cust_info';
-INSERT INTO silver.crm_cust_info(
-cst_id,
-cst_key,
-cst_firstname,
-cst_lastname,
-cst_marital_status,
-cst_gndr,
-cst_create_date
-)
-SELECT 
-cst_id,
-cst_key,
-TRIM(cst_firstname) AS cst_firstname, -- Delete unwanted spaces
-TRIM(cst_lastname) AS cst_lastname, -- Delete unwanted spaces
-CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-	 ELSE 'n/a'
-END cst_marital_status, -- Normalize marital status values to readable format
-CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-	 ELSE 'n/a'
-END cst_gndr, -- Normalize gender values to readable format
-cst_create_date
-FROM 
-(
-	SELECT
-	*,
-	ROW_NUMBER() OVER (Partition BY cst_id ORDER BY cst_create_date DESC) as flag_last
-	FROM bronze.crm_cust_info
-	WHERE cst_id IS NOT NULL
-)t WHERE flag_last = 1 -- Select te most recent record for every customer
 
--------------------------------------------------------------------
-PRINT '>> Truncating Table: silver.crm_prd_info';
-TRUNCATE TABLE silver.crm_prd_info;
-PRINT '>> Inseting Data Into: silver.crm_prd_info';
-INSERT INTO silver.crm_prd_info (
-	prd_id,
-	cat_id,
-	prd_key,
-	prd_nm,
-	prd_cost,
-	prd_line,
-	prd_start_dt,
-	prd_end_dt
-)
-SELECT
-	prd_id,
-	REPLACE(SUBSTRING (prd_key, 1, 5), '-','_') AS cat_id, -- Ersätter - med _ och transformerar för att kunna joina tabeller
-	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Behåller allt efter kategori ID för att kunna joina tabeller
-	prd_nm,
-	ISNULL(prd_cost, 0) AS prd_cost, -- NULL ersätts med 0
-	CASE UPPER(TRIM(prd_line))
-		WHEN 'S' THEN 'Other Sales'
-		WHEN 'M' THEN 'Mountain'
-		WHEN 'R' THEN 'Road'
-		WHEN 'T' THEN 'Touring'
-		ELSE 'n/a'
-		END AS prd_line, -- Ersätter kortnamn med läsbara namn och NULL med n/a
-	CAST(prd_start_dt AS DATE) AS prd_start_dt, -- Transformerar till DATE för att fåp bort onödig timestamp
-	CAST(
-		LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 
-		AS DATE
-	) AS prd_end_dt -- Ersätter slutdatum med dagen före nästa startdatum och transformerar till DATE för att fåp bort onödig timestamp
-FROM bronze.crm_prd_info
 
----------------------------------------------------------
-PRINT '>> Truncating Table: silver.crm_sales_details';
-TRUNCATE TABLE silver.crm_sales_details;
-PRINT '>> Inseting Data Into: silver.crm_sales_details';
-INSERT INTO silver.crm_sales_details(
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price
-)
-SELECT  
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL -- Hanterar felaktig data
-		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
-	END AS sls_order_dt,
-	CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL -- Hanterar felaktiga data
-		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
-	END AS sls_ship_dt,
-	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL -- Hanterar felaktiga data
-		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
-	END AS sls_due_dt,
-	CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) -- Hanterar 0, NULL, eller negativa tal
-		THEN sls_quantity * ABS(sls_price)
-		ELSE sls_sales
-END AS sls_sales,
-	sls_quantity,
-	CASE WHEN sls_price IS NULL OR sls_price <= 0 -- Hanterar NUll, 0, eller negativa tal
-		THEN ABS(sls_sales) / NULLIF(sls_quantity, 0)
-		ELSE sls_price
-END AS sls_price
-FROM bronze.crm_sales_details
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '----------------------------';
+		PRINT 'Loading the Silver layer';
+		PRINT '----------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '----------------------------';
+		
+		-- Loading the silver.crm_cust_info
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_cust_info';
+		TRUNCATE TABLE silver.crm_cust_info;
 
------------------------------------------------------------
-PRINT '>> Truncating Table: silver.erp_cust_az12';
-TRUNCATE TABLE silver.erp_cust_az12;
-PRINT '>> Inseting Data Into: silver.erp_cust_az12';
-INSERT INTO silver.erp_cust_az12(
-	cid,
-	bdate,
-	gen
-)
+		PRINT '>> Inseting Data Into: silver.crm_cust_info';
+		INSERT INTO silver.crm_cust_info(
+		cst_id,
+		cst_key,
+		cst_firstname,
+		cst_lastname,
+		cst_marital_status,
+		cst_gndr,
+		cst_create_date
+		)
+		SELECT 
+		cst_id,
+		cst_key,
+		TRIM(cst_firstname) AS cst_firstname, -- Delete unwanted spaces
+		TRIM(cst_lastname) AS cst_lastname, -- Delete unwanted spaces
+		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+			 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+			 ELSE 'n/a'
+		END cst_marital_status, -- Normalize marital status values to readable format
+		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			 ELSE 'n/a'
+		END cst_gndr, -- Normalize gender values to readable format
+		cst_create_date
+		FROM 
+		(
+			SELECT
+			*,
+			ROW_NUMBER() OVER (Partition BY cst_id ORDER BY cst_create_date DESC) as flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		)t WHERE flag_last = 1 -- Select te most recent record for every customer
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
 
-SELECT
-	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
-		ELSE cid
-	END AS cid, -- Ta bort NAS från äldre id
-	CASE WHEN bdate > GETDATE() OR bdate < '1900-01-01' THEN NULL
-		ELSE bdate
-	END AS bdate, -- Ta bort för gamla födelsedatum och födelsedatum i framtiden
-	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-		 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-	ELSE 'n/a'
-	END AS gen -- Transformera felaktiga inmatningar till korrekta och ersätt NULL med n/a
-FROM bronze.erp_cust_az12
+		SET @start_time = GETDATE();
+		PRINT '>>> Truncating Table: silver.crm_prd_info'
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>>> Inserting Data Into: silver.crm_prd_info'
+		INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING (prd_key, 1, 5), '-','_') AS cat_id, -- Ersätter - med _ och transformerar för att kunna joina tabeller
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Behåller allt efter kategori ID för att kunna joina tabeller
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost, -- NULL ersätts med 0
+			CASE UPPER(TRIM(prd_line))
+				WHEN 'S' THEN 'Other Sales'
+				WHEN 'M' THEN 'Mountain'
+				WHEN 'R' THEN 'Road'
+				WHEN 'T' THEN 'Touring'
+				ELSE 'n/a'
+				END AS prd_line, -- Ersätter kortnamn med läsbara namn och NULL med n/a
+			CAST(prd_start_dt AS DATE) AS prd_start_dt, -- Transformerar till DATE för att fåp bort onödig timestamp
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 
+				AS DATE
+			) AS prd_end_dt -- Ersätter slutdatum med dagen före nästa startdatum och transformerar till DATE för att fåp bort onödig timestamp
+		FROM bronze.crm_prd_info
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
 
---------------------------------------------------------
-PRINT '>> Truncating Table: silver.erp_loc_a101';
-TRUNCATE TABLE silver.erp_loc_a101;
-PRINT '>> Inseting Data Into: silver.erp_loc_a101';
-INSERT INTO silver.erp_loc_a101(
-	cid,
-	cntry
-)
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+		PRINT '>> Inseting Data Into: silver.crm_sales_details';
+		INSERT INTO silver.crm_sales_details(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT  
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL -- Hanterar felaktig data
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
+			END AS sls_order_dt,
+			CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL -- Hanterar felaktiga data
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
+			END AS sls_ship_dt,
+			CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL -- Hanterar felaktiga data
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) -- Transformerar int till datum
+			END AS sls_due_dt,
+			CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) -- Hanterar 0, NULL, eller negativa tal
+				THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+		END AS sls_sales,
+			sls_quantity,
+			CASE WHEN sls_price IS NULL OR sls_price <= 0 -- Hanterar NUll, 0, eller negativa tal
+				THEN ABS(sls_sales) / NULLIF(sls_quantity, 0)
+				ELSE sls_price
+		END AS sls_price
+		FROM bronze.crm_sales_details
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
+		PRINT 'CRM files Loaded';
+		PRINT '============================';
+		PRINT 'Loading ERP files';
+		PRINT '----------------------------';
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_cust_az12';
+		TRUNCATE TABLE silver.erp_cust_az12;
+		PRINT '>> Inseting Data Into: silver.erp_cust_az12';
+		INSERT INTO silver.erp_cust_az12(
+			cid,
+			bdate,
+			gen
+		)
 
-SELECT
-	REPLACE(cid, '-', '') cid,
-	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-		WHEN TRIM(cntry) IN ('USA', 'US') THEN 'United States'
-		WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-		ELSE TRIM(cntry) 
-	END AS cntry
-FROM bronze.erp_loc_a101
+		SELECT
+			CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+				ELSE cid
+			END AS cid, -- Ta bort NAS från äldre id
+			CASE WHEN bdate > GETDATE() OR bdate < '1900-01-01' THEN NULL
+				ELSE bdate
+			END AS bdate, -- Ta bort för gamla födelsedatum och födelsedatum i framtiden
+			CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+				 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+			ELSE 'n/a'
+			END AS gen -- Transformera felaktiga inmatningar till korrekta och ersätt NULL med n/a
+		FROM bronze.erp_cust_az12
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
 
---------------------------------------------------------
-PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-TRUNCATE TABLE silver.erp_px_cat_g1v2;
-PRINT '>> Inseting Data Into: silver.erp_px_cat_g1v2';
-INSERT INTO silver.erp_px_cat_g1v2(
-	id,
-	cat,
-	subcat,
-	maintenance
-)
-SELECT
-	id,
-	cat,
-	subcat,
-	maintenance
-FROM bronze.erp_px_cat_g1v2
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_loc_a101';
+		TRUNCATE TABLE silver.erp_loc_a101;
+		PRINT '>> Inseting Data Into: silver.erp_loc_a101';
+		INSERT INTO silver.erp_loc_a101(
+			cid,
+			cntry
+		)
+
+		SELECT
+			REPLACE(cid, '-', '') cid,
+			CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('USA', 'US') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry) 
+			END AS cntry
+		FROM bronze.erp_loc_a101
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
+		TRUNCATE TABLE silver.erp_px_cat_g1v2;
+		PRINT '>> Inseting Data Into: silver.erp_px_cat_g1v2';
+		INSERT INTO silver.erp_px_cat_g1v2(
+			id,
+			cat,
+			subcat,
+			maintenance
+		)
+		SELECT
+			id,
+			cat,
+			subcat,
+			maintenance
+		FROM bronze.erp_px_cat_g1v2
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '----------------------------';
+		
+		PRINT '----------------------------';
+		PRINT 'ERP files Loaded';
+		PRINT '----------------------------';
+		PRINT 'Loading Complete';
+		PRINT '----------------------------';
+		
+		SET @batch_end_time = GETDATE();
+		PRINT'>> Batch Load Duration: ' + CAST (DATEDIFF(second, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+
+	END TRY
+	BEGIN CATCH
+			PRINT '----------------------------';
+			PRINT 'Error Occured During Loading Silver Layer'
+			PRINT 'Error Message ' + ERROR_MESSAGE();
+			PRINT 'Error Message ' + CAST (ERROR_NUMBER() AS NVARCHAR);
+			PRINT 'Error Message ' + CAST (ERROR_STATE() AS NVARCHAR);
+			PRINT '----------------------------';
+	END CATCH
+	END
